09ecfffcacc231c490afa3eee962077ac524e1ac
cookiy <cookiy8511@gmail.com>
2015-05-08 18:31:58 +0800

    更新main.js

--- public/js/main.js
+++ public/js/main.js
@@ -6,20 +6,31 @@ editor.prototype.init = function(){
 	this.fetch();
 };
 editor.prototype.fetch = function(id){
-
+	//将值保存到localData里
+	this.storeData(data);
+	this.initView();
 };
 editor.prototype.save = function(layer,element,key,val){
 	/* body... */
 };
-editor.prototype.initView = function(data){
-	/* body... */
+editor.prototype.initView = function(){
+	this.renderPage();
+	this.renderArena();
+	this.renderPlant();
 };
-editor.prototype.storeData = function(){
-	/* body... */
+editor.prototype.storeData = function(data){
+	var tid = $('#product_id').val();
+	localData.set(tid+"_data",data);
 };
+//render left page
 editor.prototype.renderPage = function(){
 	/* body... */
 };
-editor.prototype.updateData = function(){
-	/* body... */
+//render middle arena
+editor.prototype.renderArena = function () {
+	// body...
+};
+//render right workshop
+editor.prototype.renderPlant = function () {
+	
 };
