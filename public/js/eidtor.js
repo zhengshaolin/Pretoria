66f0ae2c39d1b4e67272d8fcc8005b50c8eb07fa
王大力 <cookiy8511@gmail.com>
2015-05-14 22:00:26 +0800

    列表页开发

--- public/js/eidtor.js
+++ public/js/eidtor.js
@@ -9,8 +9,8 @@ var Editor = {
     //type 2 elements
     add: function(type) {
         var token = localData.get('token'),
-            product_id = $('#create_element_product_id').val(),
-            page_id = $('#create_element_page_id').val();
+            product_id = $('#product_id').val(),
+            page_id = $('#page_id').val();
         if (type = 0) {
             $.ajax({
                 type: 'POST',
@@ -101,7 +101,7 @@ var Editor = {
                 'Access-Token': token
             },
             success: function(products) {
-                console.log('fetchList returned:');
+                console.log('fetchList returned');
                 console.log(products);
                 Editor.renderList(products);
             },
@@ -131,11 +131,115 @@ var Editor = {
             this.auth();
         }
     },
-    remove: function() {
+    publish:function (id) {
+        
+    },
+    //remove method for administor delete product,page or elements
+    //type 0 product
+    //tyoe 1 page
+    //type 2 elements
+    remove: function(type) {
+        var token = localData.get('token'),
+            current_id = localData.get('current_id'),
+            product_id = $('#product_id').val(),
+            page_id = $('#page_id').val(),
+            element_id = $('#element_id').val();
+        if (type == 0) {
+            console.log('delete_product_test sended');
+            $.ajax({
+                type: 'DELETE',
+                url: 'http://115.29.32.105:8080/api',
+                data: {
+                    'type': 0,
+                    'product_id': product_id
+                },
+                dataType: 'json',
+                headers: {
+                    'Access-Token': token
+                },
+                success: function(result) {
+                    $('#delete_product_test_result').text(JSON.stringify(result));
+                    console.log('delete_product_test returned:');
+                    console.log(result);
+                    $('#'+current_id).remove();
+                },
+                error: function(err) {
+                    $('#delete_product_test_result').text(JSON.stringify(err));
+                    console.log('delete_product_test err:');
+                    console.log(err);
+                }
+            });
+        } else if (type == 1) {
+            console.log('delete_page_test sended');
+            $.ajax({
+                type: 'DELETE',
+                url: 'http://115.29.32.105:8080/api',
+                data: {
+                    'type': 1,
+                    'product_id': product_id,
+                    'page_id': page_id
+                },
+                dataType: 'json',
+                headers: {
+                    'Access-Token': token
+                },
+                success: function(result) {
+                    $('#delete_page_test_result').text(JSON.stringify(result));
+                    console.log('delete_page_test returned:');
+                    console.log(result);
+                },
+                error: function(err) {
+                    $('#delete_page_test_result').text(JSON.stringify(err));
+                    console.log('delete_page_test err:');
+                    console.log(err);
+                }
+            });
+        } else if (type == 2) {
+            console.log('delete_page_test sended');
+            $.ajax({
+                type: 'DELETE',
+                url: 'http://115.29.32.105:8080/api',
+                data: {
+                    'type': 2,
+                    'product_id': product_id,
+                    'page_id': page_id,
+                    'element_id': element_id
+                },
+                dataType: 'json',
+                headers: {
+                    'Access-Token': token
+                },
+                success: function(result) {
+                    $('#delete_element_test_result').text(JSON.stringify(result));
+                    console.log('delete_element_test returned:');
+                    console.log(result);
+                },
+                error: function(err) {
+                    $('#delete_element_test_result').text(JSON.stringify(err));
+                    console.log('delete_element_test err:');
+                    console.log(err);
+                }
+            });
+        }
+    },
+    renderIF:function () {
+        var List_current_id = localData.get('List_current_id'),
+            obj = localData.get(List_current_id+'_data');
+        $('#v_product_info').find('.status').text(obj.status);
+        $('#v_product_info').find('.avatar').attr('src',obj.avatar);
+        $('#v_product_info').find('.creat_time').text(obj.creat_time);
+        $('#v_product_info').find('.update_time').text(obj.update_time);
+        $('#v_product_info').find('.product_name').val(obj.product_name);
+        $('#v_product_info').find('.product_des').text(obj.product_des);
 
     },
     renderList: function(data) {
-        // body...
+        console.log('renderList method start');
+        $('#v_product_list').empty();
+        for (var i = 0; i < data.length; i++) {
+            localData.set(data[i]._id + '_data', data[i]);
+            $('#v_product_list').append('<li id="'+data[i]+'"><img data-holder-rendered="true" src="' + data[i].img + '" class="works-img" data-src="holder.js/180x320" alt="180x320"><div class="operation"><a class="e_edit" id="' + data[i]._id + '">编辑</a><a class="e_review id="' + data[i]._id + '">预览</a><a class="e_publish" id="' + data[i]._id + '">发布</a><a class="last e_delete" id="" type="0">删除</a></div><div class="caption">' + data[i].name + '</div></li>');
+        }
     },
     renderPage: function() {
 
--- public/js/list.js
+++ public/js/list.js
@@ -1,20 +1,25 @@
 if ($ && jQuery) {
 
     $(document).ready(function() {
-        alert(Editor.token);
         Editor.initList();
     });
 
     $('.e_creat').click(function(){
         var type = $(this).attr('type');
-        switch(type) {
-            case 0:
-                Editor.addProduct();
-                break;
-            case :
-                addPage();
-                break;
-        }
+        Editor.add(type);
+    });
+    $('.e_delete').on('click',function () {
+        var type = $(this).arrt('type');
+        Editor.remove(type);
+    });
+    $('#v_product_list li').on('click',function(){
+        var List_current_id = $(this).attr('id');
+        localData.set('List_current_id',List_current_id);
+        Editor.renderPI();
+    });
+    $('.e_publish').click(function () {
+        var List_current_id = localData.get('List_current_id');
+        Editor.publish(List_current_id);
     });
 
 };
