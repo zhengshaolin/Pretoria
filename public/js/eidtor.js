412a7b2ee288d31e16ffcad57808416c677982ab
cookiy <cookiy8511@gmail.com>
2015-06-02 00:46:17 +0800

    miss a

--- public/js/eidtor.js
+++ public/js/eidtor.js
@@ -270,7 +270,7 @@ var Editor = {
     //初始化列表页
     initList: function() {
         if (localData.get('token') != null) {
-            this.setUsername():
+            this.setUsername();
             this.fetchList();
         } else {
             this.auth();
@@ -280,7 +280,7 @@ var Editor = {
     initForm: function() {
         $('#product_id').val(window.location.href.split('?')[1].split('=')[1]);
         if (localData.get('token') != null) {
-            this.setUsername():
+            this.setUsername();
             this.fetchForm(0);
             this.renderPicBox();
         } else {
